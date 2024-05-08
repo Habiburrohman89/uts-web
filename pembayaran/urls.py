f9a2058a88044cbdf179d1653cb1a599b779443a
Habiburrohman89 <kenzoohabib@gmail.com>
2024-05-08 18:20:35 +0700

    menambahkan fungsi  @api_view(['GET', 'PUT', 'DELETE']) pada views module pembayaran

--- pembayaran/urls.py
+++ pembayaran/urls.py
@@ -5,4 +5,5 @@ from pembayaran import views
 
 urlpatterns = [
     path('pembayaran/',views.pembayaran_list),
+    path('pembayaran/<int:pk>/', views.pembayaran_detail),
 ]
--- pembayaran/views.py
+++ pembayaran/views.py
@@ -6,7 +6,7 @@ from .serializers import PembayaranSerializer
 
 
 @api_view(['GET', 'POST'])
-def pembayaran_list(request):
+def pembayaran_list(request,format=None):
     if request.method == 'GET':
         pembayaran = Pembayaran.objects.all()
         serializer = PembayaranSerializer(pembayaran, many=True)
@@ -18,3 +18,26 @@ def pembayaran_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def pembayaran_detail(request, pk, format=None):
+    try:
+        pembayaran = Pembayaran.objects.get(pk=pk)
+    except Pembayaran.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = PembayaranSerializer(pembayaran)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = PembayaranSerializer(pembayaran, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        pembayaran.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
